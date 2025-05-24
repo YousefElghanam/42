b5547b470532fb09eb1427c6deca38b01cfbb732
Yousef Elghanam <104347261+YousefElghanam@users.noreply.github.com>
2025-05-24 11:51:56 +0200

    Update main.c

--- push_swap/main.c
+++ push_swap/main.c
@@ -3,7 +3,26 @@
 int	main(int argc, char **argv)
 {
 	validate_args((size_t)argc, argv);
-    init_stack_a((size_t)argc, argv);
+    init_stack_a((size_t)argc - 1, argv);
     init_stack_b();
     solve();
+}
+
+typedef struct s_node
+{
+    int num;
+    int is_top;
+    int position;
+    struct s_node next;
+} t_node;
+
+typdef struct s_stack
+{
+    int size;
+    t_node top;
+}
+
+void init_stack_a(size_t n_count, char **argv)
+{
+    
 }
